8a1ce022d4ed303ab63f96997b5d0e39b724f5e8
andreeezinho <andreeesapucaia@gmail.com>
2025-03-31 20:30:54 -0300

    chore: create transform_data main

---
+++ DataTransformation/main.py
@@ -0,0 +1,14 @@
+import os
+
+class transformData:
+    def __init__(self):
+        pdf_file = "Anexo_I_Rol_2021RN_465.2021_RN627L.2024.pdf"
+        pdf_path = os.path.join('../Webscraping/downloads', pdf_file)
+
+        self.getDataTransformed(pdf_path)
+
+    def getDataTransformed(self, pdf_path):
+        
+        
+if __name__ == '__main__':
+    transformData()
---
+++ DataTransformation/transform.py
@@ -0,0 +1,7 @@
+import os
+import tabula
+
+class transform:
+    def transformDataInCsv(pdf_path):
+        if pdf_path != None:
+             
