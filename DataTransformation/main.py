c38f2e125172b868b65fee4221404c1302a06ef6
andreeezinho <andreeesapucaia@gmail.com>
2025-03-31 20:47:34 -0300

    feat: create transform data in csv method

--- DataTransformation/main.py
+++ DataTransformation/main.py
@@ -1,6 +1,9 @@
 import os
 
+from transform import transform
+
 class transformData:
+
     def __init__(self):
         pdf_file = "Anexo_I_Rol_2021RN_465.2021_RN627L.2024.pdf"
         pdf_path = os.path.join('../Webscraping/downloads', pdf_file)
@@ -8,7 +11,10 @@ class transformData:
         self.getDataTransformed(pdf_path)
 
     def getDataTransformed(self, pdf_path):
-        
+        if transform.dataInCsv(pdf_path) == True:
+            transform.renameLines()
+
+        return False
         
 if __name__ == '__main__':
     transformData()
--- DataTransformation/transform.py
+++ DataTransformation/transform.py
@@ -2,6 +2,13 @@ import os
 import tabula
 
 class transform:
-    def transformDataInCsv(pdf_path):
+    def dataInCsv(pdf_path):
         if pdf_path != None:
-             
+            tabula.convert_into(pdf_path, "Arquivo.csv", output_format="csv", pages="all", stream=True)
+            print("Tabela CSV salva")
+            return True
+        
+        return False
+    
+    def renameLines():
+        csv_file = open("Arquivo.csv", mode="r+")
